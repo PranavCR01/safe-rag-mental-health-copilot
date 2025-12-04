31f66ec04f0e12933574c2ec731223c2d1549a78
srg8 <srg8@illinois.edu>
2025-12-03 22:20:08 -0600

    New push

--- core/tone.py
+++ core/tone.py
@@ -1,14 +1,3 @@
-#core/tone.py
-
-# def empathy_level(msg: str) -> int:
-#     m = msg.lower()
-#     score = 1
-#     if any(k in m for k in ["very anxious","panic","cry","shaking","can’t sleep","overwhelmed"]): score += 1
-#     if any(k in m for k in ["hopeless","worthless","numb"]): score += 1
-#     return min(score, 3)
-
-#core/tone.py
-
 from typing import Dict, List
 
 
